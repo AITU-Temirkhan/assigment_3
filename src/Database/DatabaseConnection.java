084d8fb5687ae6912687d32c704509eb50c81d7a
assignment1 <riftlik@gmail.com>
2026-01-22 11:04:40 +0500

    Kairatuly Miras connect Database

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.eatwldgvgzxpyolzapzm";
+    private static final String PASSWORD = "EQQUWWQW_31172212";
+
+    private DatabaseConnection() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
