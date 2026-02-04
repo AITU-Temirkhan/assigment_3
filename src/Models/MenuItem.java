eada0df89c1e694ae6b2c73c3d5c76c574d429fb
assignment1 <riftlik@gmail.com>
2026-02-04 22:05:56 +0500

    Kairatuly Miras add new Models

--- src/Models/MenuItem.java
+++ src/Models/MenuItem.java
@@ -5,6 +5,7 @@ public class MenuItem {
     private String name;
     private double price;
     private boolean available;
+    private String category;
 
     public MenuItem(int id, String name, double price, boolean available) {
         this.id = id;
@@ -13,9 +14,25 @@ public class MenuItem {
         this.available = available;
     }
 
+    public MenuItem(int id, String name, double price, boolean available, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.available = available;
+        this.category = category;
+    }
+
     public int getId() { return id; }
     public String getName() { return name; }
     public double getPrice() { return price; }
     public boolean isAvailable() { return available; }
-}
+    public String getCategory() { return category; }
 
+    public void setAvailable(boolean available) { this.available = available; }
+
+    @Override
+    public String toString() {
+        return String.format("MenuItem{id=%d, name='%s', price=%.2f, available=%s}",
+                id, name, price, available);
+    }
+}
