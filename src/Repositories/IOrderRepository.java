10a850c552c6e361ad958edcda6091f5477e7cb2
assignment1 <riftlik@gmail.com>
2026-02-04 12:46:26 +0500

    Kairatuly Miras delete dont usable classes

--- src/Repositories/IOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package Repositories;
-
-import Models.OrderItem;
-import java.util.List;
-
-public interface IOrderRepository {
-    void saveOrder(int customerId, List<OrderItem> items) throws Exception;
-}
